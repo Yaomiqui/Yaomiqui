01a683cf4d551cc026b6cff0cdcb1c9d6295be68
Hugo Maza <hugo.maza@gmail.com>
2018-11-03 20:58:23 -0600

    Pre-release

--- html/Crypt/Babel.pm
+++ html/Crypt/Babel.pm
@@ -22,8 +22,8 @@ $LASTEDIT = "04/08/05";
 
 require Exporter;
 @ISA       = qw(Exporter);
-@EXPORT    = qw(encode new version modulename lastedit);
-@EXPORT_OK = qw(encode new version modulename lastedit);
+@EXPORT    = qw(encode decode new version modulename lastedit);
+@EXPORT_OK = qw(encode decode new version modulename lastedit);
 
 sub new {
     my    $object = {};
@@ -100,6 +100,67 @@ sub encode {
        $_w =~ s/f/\^/g;
 
        return reverse($_w);    
+}
+
+sub decode {
+    shift;
+    local ($_P1)= @_;
+    shift;
+    local ($_K1)= @_;
+    
+    $_P1 = reverse($_P1);
+
+    $_P1 =~ s/\./a/g;
+    $_P1 =~ s/-/b/g;
+    $_P1 =~ s/\+/c/g;
+    $_P1 =~ s/\!/d/g;
+    $_P1 =~ s/\*/e/g;
+    $_P1 =~ s/\^/f/g;
+
+    my @_p = ();
+    my @_k = ();
+    my @_e = ();
+    my $_l = "";
+    my $_i = 0;
+    my $_j = 0;
+    my $_r = "";
+    my $_t = 0;
+    my $_h = 0;
+    my $_o = 0;
+    my $_d = 0;
+    my @_w1= ();
+    my $_w2= "";
+
+    for($_d=0;$_d < length($_P1);$_d=$_d+2) {
+        $_w1[$_d]=chr(hex(substr($_P1,$_d,2)));
+                                           }
+    $_w2=join '',@_w1;
+
+    $_P1=$_w2;
+
+    while ( length($_K1) < length($_P1) ) { $_K1=$_K1.$_K1;}
+
+    $_K1=substr($_K1,0,length($_P1));
+
+    @_p=split(//,$_P1);
+    @_k=split(//,$_K1);
+
+    while ( $_i < scalar(@_p) ) {
+            $_o = ord($_p[$_i]);
+            $_h = ord($_p[$_i+1]);
+            $_o = $_o ^ ord ($_k[$_j]);
+            $_h = $_h ^ ord ($_k[$_j]);
+            $_h = $_h * 256;
+            $_l = $_h + $_o;
+            $_l = $_l / ord ($_k[$_j]);
+            $_e[$_j] = chr($_l);
+            $_j++;
+            $_i=$_i+2;
+                                }
+                      
+    $_r = join '',@_e;
+
+    return $_r;    
 }
 
 1;
